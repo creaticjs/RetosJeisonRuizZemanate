df93a9f564555b1729b4e5f42db11d87d5662e0e
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2018-10-01 21:39:29 -0500

    Ultimo commit Select Localizacion y FIREBASE 🔥📡🔥

--- ApiGpsHere/climaHere.js
+++ ApiGpsHere/climaHere.js
@@ -24,38 +24,93 @@ $(function(){
     $('select').formSelect();
   });
 
+  /////
+  var selCiudad = document.getElementById("selCiudad");
+
+  selCiudad.onchange = function ()
+  {
+   //document.getElementById("nombre").innerHTML = this.value;
+  //console.log();
+  var nomCiudad=this.options[this.selectedIndex].text;
+  
+  console.log(nomCiudad); 
+
+  var nomCSelec=nomCiudad.toLowerCase();;
+  console.log(nomCSelec); 
+
+  function getCleanedString(cadena){
+    // Definimos los caracteres que queremos eliminar
+    var specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.";
+ 
+    // Los eliminamos todos
+    for (var i = 0; i < specialChars.length; i++) {
+        cadena= cadena.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+    }   
+ 
+    // Lo queremos devolver limpio en minusculas
+    cadena = cadena.toLowerCase();
+ 
+    // Quitamos espacios y los sustituimos por _ porque nos gusta mas asi
+    cadena = cadena.replace(/ /g,"_");
+ 
+    // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
+    cadena = cadena.replace(/á/gi,"a");
+    cadena = cadena.replace(/é/gi,"e");
+    cadena = cadena.replace(/í/gi,"i");
+    cadena = cadena.replace(/ó/gi,"o");
+    cadena = cadena.replace(/ú/gi,"u");
+    cadena = cadena.replace(/ñ/gi,"n");
+    return cadena;
+ }
+
+
+ nomCSelec = getCleanedString(nomCSelec);
+ console.log(nomCSelec); 
+
+  if(nomCSelec =="popayan")
+  {
+
+    peticion('https://weather.cit.api.here.com/weather/1.0/report.json')
+    .then(function(data){
+        console.log(data);
+
+        var ciudad= document.getElementById('ciudad');
+        var ciudad2= document.getElementById('ciudad2');
+        var ciudad3= document.getElementById('ciudad3');
+
+        var climita= data.observations.location[0];
+        console.log(climita);
+
+        var auxC = "";
+        var auxC2 = "";
+        var auxC3 = "";
+        auxC += climita.city+"<i class='material-icons right '> more_vert</i> </span>";
+        auxC2 += climita.city+"<i class='material-icons right'>close</i></span>";
+        
+        auxC3 += "<br> Pais: "+climita.country+"<br>Distancia: "+climita.distance+"<br>Latitud: "+climita.latitude+"<br>Longitud: "+climita.longitude+"<br>Departamento: "
+        +climita.state +"<br>Zona Horaria: "+climita.timezone+"<br>";
+       
+     
+       ciudad.innerHTML =  auxC ;
+       ciudad2.innerHTML =  auxC2 ;
+       ciudad3.innerHTML =  auxC3 ;
+
+    })
+    .catch(function(err){
+     console.log(err);
+    })
+
+  
+
+  }
+}
+
+
+
+
    //$('#saludo').click(saludito);
    //$('#saludo').on('click',function(){
-       peticion('https://weather.cit.api.here.com/weather/1.0/report.json')
-       .then(function(data){
-           console.log(data);
-
-           var ciudad= document.getElementById('ciudad');
-           var ciudad2= document.getElementById('ciudad2');
-           var ciudad3= document.getElementById('ciudad3');
-
-           var climita= data.observations.location[0];
-           console.log(climita);
-
-           var auxC = "";
-           var auxC2 = "";
-           var auxC3 = "";
-           auxC += climita.city+"<i class='material-icons right '> more_vert</i> </span>";
-           auxC2 += climita.city+"<i class='material-icons right'>close</i></span>";
-           
-           auxC3 += "<br> Pais: "+climita.country+"<br>Distancia: "+climita.distance+"<br>Latitud: "+climita.latitude+"<br>Longitud: "+climita.longitude+"<br>Departamento: "
-           +climita.state +"<br>Zona Horaria: "+climita.timezone+"<br>";
-          
-        
-          ciudad.innerHTML =  auxC ;
-          ciudad2.innerHTML =  auxC2 ;
-          ciudad3.innerHTML =  auxC3 ;
-
-       })
-       .catch(function(err){
-        console.log(err);
-       })
-   
+      
 });
 
 
@@ -106,3 +161,4 @@ reject("ERROR 🎇")
 
 
 }
+
